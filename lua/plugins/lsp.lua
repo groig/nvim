11ef60c527863ceb893a1655661977696e821fbc
Roig <groig@protonmail.com>
2024-05-16 17:57:54 -0400

    luals

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -91,6 +91,31 @@ return {
       single_file_support = true,
     }
 
+    require("lspconfig").lua_ls.setup({
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+            },
+          },
+        })
+      end,
+      settings = {
+        -- Lua = { hint = { enable = true } },
+        Lua = {},
+      },
+    })
+
     lspconfig.lexical.setup(vim.tbl_extend("force", lsp_options, {
       cmd = { "/home/roig/src/lexical/_build/prod/package/lexical/bin/start_lexical.sh" },
     }))
