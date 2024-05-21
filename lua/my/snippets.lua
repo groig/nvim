73c417792ef4c5763bdae02e89738c5fbf7f1fb1
Roig <groig@protonmail.com>
2024-05-21 19:18:11 -0400

    add snippets

--- lua/my/snippets.lua
+++ lua/my/snippets.lua
@@ -15,12 +15,38 @@ local global_snippets = {
       return os.date("%Y/%m/%d %H:%M:%S")
     end,
   },
+  {
+    trigger = "todo",
+    body = function()
+      return vim.bo.commentstring:gsub("%%s", "TODO: ")
+    end,
+  },
+  { trigger = "lorem", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+  {
+    trigger = "uuid",
+    body = function()
+      local random = math.random
+      local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+      return string.gsub(template, "[xy]", function(c)
+        local v = (c == "x") and random(0, 0xf) or random(8, 0xb)
+        return string.format("%x", v)
+      end)
+    end,
+  },
 }
 
 local snippets_by_filetype = {
   python = {
     { trigger = "pudb", body = "import pudb; pudb.set_trace()" },
     { trigger = "docs", body = '"""\n$0\n"""' },
+    {
+      trigger = "timer",
+      body = "import time\nstart_time = time.time()\n$0\nprint('Execution Time:', time.time() - start_time)",
+    },
+    { trigger = "nq", body = "# noqa" },
+  },
+  javascript = {
+    { trigger = "cl", body = "console.log($0);" },
   },
 }
 
