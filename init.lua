1db146d5f82ac125ef9e241036d36a8fec34a79e
Roig <groig@protonmail.com>
2024-05-27 11:47:17 -0400

    vim.loop -> vim.uv

--- init.lua
+++ init.lua
@@ -182,7 +182,7 @@ autocmd("BufReadPost", {
 
 -- plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
