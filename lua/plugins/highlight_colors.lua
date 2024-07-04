2260a63ce87fb5eddc74a9886dbf993ed8f32e10
Roig <groig@protonmail.com>
2024-07-04 10:08:34 -0400

    nvim-highlight-colors -> mini.hipatterns

--- lua/plugins/highlight_colors.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "brenoprata10/nvim-highlight-colors",
-  config = function()
-    require("nvim-highlight-colors").setup({})
-  end,
-  event = "BufReadPost",
-}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,6 +10,16 @@ return {
         animation = require("mini.indentscope").gen_animation.none(),
       },
     })
+    local hipatterns = require("mini.hipatterns")
+    hipatterns.setup({
+      highlighters = {
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    })
   end,
   event = "BufReadPost",
 }
