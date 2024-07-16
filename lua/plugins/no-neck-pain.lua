179c9f8eb914ecd491c81b8fd316001ff905e9c6
Roig <groig@protonmail.com>
2024-07-16 18:18:57 -0400

    no-neck-pain

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,32 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+
+  keys = {
+    "<leader>n",
+  },
+
+  config = function()
+    local nnp = require("no-neck-pain")
+    nnp.setup({
+      width = 180,
+      buffers = {
+        left = {
+          scratchPad = {
+            enabled = true,
+            pathToFile = "./TODO",
+          },
+          bo = {
+            filetype = "md",
+          },
+        },
+        wo = {
+          fillchars = "eob: ",
+        },
+      },
+    })
+    local map = vim.keymap.set
+    map("n", "<leader>n", function()
+      nnp.toggle()
+    end, { noremap = true })
+  end,
+}
