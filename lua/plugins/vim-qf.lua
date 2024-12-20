8f74aca17859e39241a2f2d4c5b7ad26b517d9a8
Roig <groig@protonmail.com>
2024-12-20 12:44:58 -0500

    qf maps

--- lua/plugins/vim-qf.lua
+++ lua/plugins/vim-qf.lua
@@ -2,17 +2,17 @@ local map = vim.keymap.set
 return {
   {
     "romainl/vim-qf",
-    event = "CmdlineEnter",
+    event = "BufReadPost",
     config = function()
       vim.g["qf_mapping_ack_style"] = 1
       vim.g["qf_statusline"] = {
         before = [[%<\ ]],
         after = [[\ %f%=%l\/%-6L\ \ \ \ \ ]],
       }
-      map("n", "[w", "<Plug>(qf_loc_previous)", { noremap = false })
-      map("n", "]w", "<Plug>(qf_loc_next)", { noremap = false })
-      map("n", "[q", "<Plug>(qf_qf_previous)", { noremap = false })
-      map("n", "]q", "<Plug>(qf_qf_next)", { noremap = false })
+      map("n", "<A-k>", "<Plug>(qf_loc_previous)", { noremap = false })
+      map("n", "<A-j>", "<Plug>(qf_loc_next)", { noremap = false })
+      map("n", "<C-k>", "<Plug>(qf_qf_previous)", { noremap = false })
+      map("n", "<C-j>", "<Plug>(qf_qf_next)", { noremap = false })
     end,
   },
 }
