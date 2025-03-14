0b9d2aed088b357024b717803b7c4525ee4b1aae
Roig <groig@protonmail.com>
2025-03-14 09:30:55 -0400

    fix mapping

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ autocmd("LspAttach", {
     client.server_capabilities.semanticTokensProvider = nil
     local opts = { noremap = true, silent = true, buffer = args.buf }
     map("n", "<leader>a", vim.lsp.buf.code_action, opts)
-    map("n", "<leader-f>", vim.lsp.buf.references, opts)
+    map("n", "<leader>f", vim.lsp.buf.references, opts)
     map("n", "<leader>e", vim.diagnostic.open_float, opts)
     map("n", "<leader>l", vim.diagnostic.setloclist, opts)
     map("n", "<leader>d", function()
