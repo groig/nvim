56da14e5490ae2c36c1ac1ce56c1ad20467905be
Roig <groig@protonmail.com>
2026-04-25 20:59:43 -0400

    blink v2

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,11 @@
 return {
   "saghen/blink.cmp",
-  build = "cargo build --release",
+  dependencies = {
+    "saghen/blink.lib",
+  },
+  build = function()
+    require("blink.cmp").build():wait(60000)
+  end,
   event = "InsertEnter",
   config = function()
     require("blink.cmp").setup({
